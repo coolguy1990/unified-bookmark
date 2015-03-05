55d6080aae804c2233b8523223771bc492102ea7
Kunal Mangaraj <mangaraj.kunal@gmail.com>
2015-03-05 18:24:47 +0530

    testing setup for mozilla bookmarks

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ Class Bookmark
         $this->url = $url;
     }
 
+    //getting bookmarks from a html file (chrome)
     public function xyz()
     {
         $data = file_get_html($this->url);
@@ -35,3 +36,31 @@ Class Bookmark
 $data = new Bookmark('path/to/html/file');
 
 var_dump($data->xyz());
+
+
+//getting bookmarks from chrome default storage directly
+//storage path on linux -> /home/user/.config/google-chrome/Default/Bookmarks
+// $data = json_decode(file_get_contents('./Bookmarks'));
+
+// $bookmarks = $data->roots->bookmark_bar->children;
+
+// foreach($bookmarks as $b)
+// {
+//     $result[$b->name] = $b->url;
+// }
+
+// var_dump($result);
+
+//getting bookmarks from firefox default storage
+//storage path on linux -> /home/.mozilla/firefox/xxxxx.default/places.sqlite
+// $db = new SQLite3('places.sqlite');
+// $results = $db->query('Select moz_bookmarks.title, moz_places.url from moz_bookmarks join moz_places on moz_places.id=moz_bookmarks.fk');
+// while ($row = $results->fetchArray())
+// {
+//     if(substr($row['url'], 0, 4) == 'http')
+//     {
+//         $test[$row['title']] = $row['url'];
+//     }
+// }
+
+var_dump($test);
