547fb2e5f59e57dcc2b79a30751d7531cedfbb54
Kunal Mangaraj <mangaraj.kunal@gmail.com>
2018-03-20 12:39:42 +0530

    added logging

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,7 @@
 const url = require('url');
 const path = require('path');
+const { createLogger, transports, format } = require('winston');
+const { combine, timestamp, colorize, simple, json } = format;
 
 const getFavicon = (link) => {
   if (link && link.length) {
@@ -26,8 +28,35 @@ const resolve = (dir) => {
   return path.join(__dirname, '..', dir);
 }
 
+const logger = createLogger({
+  level: 'info',
+  format: combine(
+    timestamp(),
+    json()
+  ),
+  transports: [
+    new transports.File({ filename: resolve('src/logs/error.log'), level: 'error' }),
+    new transports.File({ filename: resolve('src/logs/combined.log') })
+  ],
+  exceptionHandlers: [
+    new transports.File({ filename: resolve('src/logs/exceptions.log') })
+  ]
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new transports.Console({
+    format: combine(
+      timestamp(),
+      colorize({all: true}),
+      simple()
+    ),
+    handleExceptions: true
+  }));
+}
+
 module.exports = {
   getFavicon,
   removeDuplicates,
-  resolve
+  resolve,
+  logger
 };
